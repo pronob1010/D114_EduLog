76fcad67ed8ee584bd2c80826e72424637a82e03
pronob1010 <pronobmozumder.info@gmail.com>
2021-09-08 04:04:27 +0600

    site info added v2

--- EduBackend/siteinfo/models.py
+++ EduBackend/siteinfo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
- 
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class SiteBanner(models.Model):
@@ -31,3 +31,16 @@ class Official_Team(models.Model):
 
     def __str__(self):
         return self.Member.username +" | "+self.Designation
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=50, null=True, blank=True)
+    answer = models.TextField(max_length=150, null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.question+"-"+str(self.id))
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.question
